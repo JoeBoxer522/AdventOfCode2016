44c56ae6e0db4ad21c0fe2e4acadcb4a2d4743e8
JoeyManso <jmanso522@gmail.com>
2016-12-09 17:55:56 -0500

    Completion of Day 9

--- Source/Day09.cpp
+++ Source/Day09.cpp
@@ -4,7 +4,7 @@
 // Repeat Token is (#x#)
 const static regex RX_REPEAT(R"(\((\d+)x(\d+)\))", regex::optimize);
 
-size_t Decompress(string arg, bool decompressDeep)
+size_t Decompress(const string& arg, bool decompressDeep)
 {
     smatch match;
     if(!regex_search(arg, match, RX_REPEAT))
@@ -14,29 +14,29 @@ size_t Decompress(string arg, bool decompressDeep)
     size_t len = 0;
 
     // Parse the range of the chars to repeat and the loop count
-    size_t charRange = stoi(match.str(1));
-    size_t repeatCount = stoi(match.str(2));
+    size_t repeatCharLen = stoi(match.str(1));
+    size_t repeatCharLoops = stoi(match.str(2));
 
     // Decompress the substring if requested
-    size_t substrIdx = match.position() + match.length();
+    size_t matchIdx = match.position() + match.length();
     if(decompressDeep)
     {
-        const string subStr = arg.substr(substrIdx, charRange);
-        len += Decompress(subStr, decompressDeep) * repeatCount;
+        const string subStrDeep = arg.substr(matchIdx, repeatCharLen);
+        len += Decompress(subStrDeep, decompressDeep) * repeatCharLoops;
     }
     else
     {
-        len += charRange * repeatCount;
+        len += repeatCharLen * repeatCharLoops;
     }
-    const string subStr = arg.substr(substrIdx + charRange);
+    const string subStr = arg.substr(matchIdx + repeatCharLen);
     len += Decompress(subStr, decompressDeep);
     return len;
 }
 
 template <>
-void Run<Day09>(Part part, istream& is, std::ostream& os)
+void Run<Day09>(Part part, istream& is, ostream& os)
 {
-    std::string arg(std::istreambuf_iterator<char>(is), {});
+    string arg(istreambuf_iterator<char>(is), {});
     size_t dcCount = Decompress(arg, part == Part02);
     os << "Compressed string length: " << arg.length() << endl;
     os << "Decompressed string length: " << dcCount << endl;
